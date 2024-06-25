9dd3b58069906b26de8d24393de6fcae43348de1
Monteguss <otaviomarquesbr@gmail.com>
2024-06-25 18:34:58 -0300

    Correção sidebar com mudanças de seção

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('.container > .section');
     const sideBarItems = document.querySelectorAll('.side-nav-bar .side-bar-item'); // Seleciona os itens da barra lateral
-    let currentSection = 0;
+    let currentIndex = 0;
+
+    // Garante que as classes das seções estejam corretas
+    function classTreatment(bufferIndex) {
+        if (currentIndex > bufferIndex) {
+            for (i = bufferIndex + 1; i < currentIndex; i++) {
+                sections[i].setAttribute('data-status', 'inactive-bottom');
+            }
+        }
+
+        if (currentIndex < bufferIndex) {
+            for (i = currentIndex + 1; i < bufferIndex; i++) {
+                sections[i].setAttribute('data-status', 'inactive-top');
+            }
+        }
+    }
+
+    function updateSection(bufferIndex) {
+        if (currentIndex < bufferIndex) {
+            sections[currentIndex].setAttribute('data-status', 'inactive-top');
+            sections[bufferIndex].setAttribute('data-status', 'active');
+        }
+
+        if (currentIndex > bufferIndex) {
+            sections[currentIndex].setAttribute('data-status', 'inactive-bottom');
+            sections[bufferIndex].setAttribute('data-status', 'active');
+        }
+    }
+
+    function updateSidebar(bufferIndex) {
+        sideBarItems[currentIndex].setAttribute('data-status', 'inactive');
+        sideBarItems[bufferIndex].setAttribute('data-status','active');
+    }
 
     // Função para definir a seção ativa
-    function setActiveSection(index) {
-        sideBarItems[currentSection].setAttribute('data-status', 'inactive');
-        sideBarItems[index].setAttribute('data-status','active');
-        sections[currentSection].setAttribute('data-status', 'inactive');
-        sections[index].setAttribute('data-status', 'active');
-        currentSection = index;
+    function setActive(bufferIndex) {
+
+        classTreatment(bufferIndex);
+        updateSidebar(bufferIndex);
+        updateSection(bufferIndex);
+        
+        currentIndex = bufferIndex;
     }
 
     // Configuração para clicar nos itens da barra lateral
     sideBarItems.forEach((item, index) => {
         item.addEventListener('click', () => {
-            setActiveSection(index);
+            setActive(index);
         });
     });
 
     // Define a seção inicial como ativa
-    setActiveSection(0);
+    setActive(0);
 });
