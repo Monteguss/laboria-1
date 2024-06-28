0976590d7f8e691dec474a8a8d1e8a82c6b3eae6
Monteguss <otaviomarquesbr@gmail.com>
2024-06-28 18:12:02 -0300

    Scroll + animação + footer

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const sections = document.querySelectorAll('.container > .section');
-    const sideBarItems = document.querySelectorAll('.side-nav-bar .side-bar-item'); // Seleciona os itens da barra lateral
+    const sections = document.querySelectorAll('.main-section');
+    const sideBarItems = document.querySelectorAll('.side-bar-item');
     let currentIndex = 0;
+
+    let isScrolling = false;
+    const scrollDelay = 450;
 
     // Garante que as classes das seções estejam corretas
     function classTreatment(bufferIndex) {
         if (currentIndex > bufferIndex) {
-            for (i = bufferIndex + 1; i < currentIndex; i++) {
+            for (let i = bufferIndex + 1; i < currentIndex; i++) {
                 sections[i].setAttribute('data-status', 'inactive-bottom');
             }
         }
 
         if (currentIndex < bufferIndex) {
-            for (i = currentIndex + 1; i < bufferIndex; i++) {
+            for (let i = currentIndex + 1; i < bufferIndex; i++) {
                 sections[i].setAttribute('data-status', 'inactive-top');
             }
         }
@@ -45,11 +48,39 @@ document.addEventListener('DOMContentLoaded', () => {
         currentIndex = bufferIndex;
     }
 
-    // Configuração para clicar nos itens da barra lateral
+    // Função para ativar itens da barra lateral
     sideBarItems.forEach((item, index) => {
         item.addEventListener('click', () => {
+            if (isScrolling) return;
+            
             setActive(index);
+
+            isScrolling = true;
+            setTimeout(() => isScrolling = false, scrollDelay);
         });
+    });
+
+    // Função detectar scroll
+    window.addEventListener('wheel', function(event) {
+        if (isScrolling) return;
+
+        if (event.deltaY > 0) {
+            if (currentIndex < sections.length - 1) {
+                setActive(currentIndex + 1);
+                isScrolling = true; // Bloqueia a rolagem
+                setTimeout(() => isScrolling = false, scrollDelay); // Desbloqueia após o atraso
+            }
+
+            console.log('Scroll Down'); // Só pra debug
+        } else {
+            if (currentIndex > 0) {
+                setActive(currentIndex - 1);
+                isScrolling = true;
+                setTimeout(() => isScrolling = false, scrollDelay);
+            }
+
+            console.log('Scroll Up');
+        }
     });
 
     // Define a seção inicial como ativa
